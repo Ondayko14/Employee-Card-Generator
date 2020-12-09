f74e3d2bbbc21336b2652b6821f6e9cdea1d8b97
Brandon O <b.ondayko@gmail.com>
2020-12-09 14:40:37 -0600

    All Classes are now set up

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,9 @@
+const { expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 
-test('see if the Engineer object is being made', () =>{
-    const manager = new Manager('Tom');
+test('see if the Manager object is being made', () =>{
+    const manager = new Manager(12);
 
-    expect(manager.name).toBe('Tom');
+    expect(manager.officeNumber).toEqual(12);
+    expect(manager.getRole()).toBe('Manager');
 });
